fd5094c50a017e1da986b366ae5751bbd61dc628
funketh <theodor.k.funke@gmail.com>
2019-04-24 13:16:13 +0200

    added progress bar, license, more cmd line args, password input, switched from selenium to aiohttp

--- mal_user_scraper.py
+++ mal_user_scraper.py
@@ -1,19 +1,73 @@
+# The MIT License (MIT)
+# Copyright (c) 2019 Theodor Funke
+# Copyright (c) 2016 Vladimir Ignatev
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the Software
+# is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+# FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+# OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+# OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 # NOTE: This script only scrapes Anime related data
+import asyncio
+import logging
 import re
 import sqlite3
-import time
-import traceback
+import sys
 from argparse import ArgumentParser
 from collections import namedtuple
 from datetime import datetime, timedelta
-from typing import Optional, List
+from getpass import getpass
+from typing import Optional
 
-from selenium.webdriver import Firefox, FirefoxProfile
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
+import aiohttp
 
-def main():
+async def main():
+    logging.basicConfig(filename='info.log', level=logging.INFO)
+    args = parse_cmd_args()
+    async with aiohttp.ClientSession() as session:
+        await login(session)
+        search_jobs = [users_from_search_page(session, args.pages, args.name, args.location,
+                                              args.older, args.younger, args.gender)]
+        user_urls = ['https://myanimelist.net/profile' + url
+                     for sublist in await asyncio.gather(*search_jobs) for url in sublist]
+        user_jobs = [page_text(session, url) for url in user_urls]
+        user_pages = await asyncio.gather(*user_jobs)
+        total = len(user_urls)
+        users = []
+        for i, page in enumerate(user_pages):
+            try:
+                users.append(get_user_data(page))
+            except Exception:
+                logging.exception('Ignoring exception: ')
+            progress(i, total, f"Scraped {users[-1].name}'s profile")
+    save_to_db(args.db, users)
+
+
+# Interface
+def parse_cmd_args():
     parser = ArgumentParser()
+    parser.add_argument("-n", "--name", dest="name", type=str, default='',
+                        help="found users names must match this")
+    parser.add_argument("-o", "--older", dest="older", type=int, default=0,
+                        help="found users must be older than this (in years)")
+    parser.add_argument("-y", "--younger", dest="younger", type=int, default=0,
+                        help="found users must be younger than this (in years)")
+    parser.add_argument("-l", "--location", dest="location", type=str, default='',
+                        help="found users must live here")
+    parser.add_argument("-g", "--gender", dest="gender", type=str, default='irrelevant',
+                        help="found users must be of this gender_id (0=irrelevant, 1=male, 2=female, 3=non-binary}")
     parser.add_argument("-p", "--pages", dest="pages", type=int, default=1,
                         help="how many pages of users to scrape (1 page = 24 users)")
     parser.add_argument("-d", "--delay", dest="delay", type=float, default=0.3,
@@ -21,56 +75,49 @@ def main():
     parser.add_argument("-db", "--database",
                         action="store_false", dest="db", default='users.db',
                         help="file path of the sqlite3 database")
-    args = parser.parse_args()
-    profile = FirefoxProfile()
-    # Disable images
-    profile.set_preference('permissions.default.image', 2)
-    driver = Firefox(profile)
-    login(driver)
-    search(driver)
-    user_urls = [time.sleep(args.delay) or url
-                 for page in fetch_pages(driver, args.pages)
-                 for url in fetch_users(page)]
-    users = []
-    for url in user_urls:
-        try:
-            users.append(get_user_data(driver, url))
-        except Exception:
-            traceback.print_exc()
-            print('\nIgnoring exception (url=' + url + ')')
-        time.sleep(args.delay)
-    save_to_db(args.db, users)
+    return parser.parse_args()
 
-def login(driver):
-    login_url = 'https://myanimelist.net/login.php'
-    driver.get(login_url)
-    print("Please log into your Account manually.")
-    WebDriverWait(driver, 600).until(EC.url_changes(login_url))
+def progress(count, total, status=''):
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+    sys.stdout.flush()
 
-def search(driver):
-    search_url = 'https://myanimelist.net/users.php'
-    driver.get(search_url)
-    print("Specify some search constraints and click 'Find user'.")
-    WebDriverWait(driver, 600).until(EC.url_changes(search_url))
-    time.sleep(3)
 
-def fetch_pages(driver, page_count):
-    yield driver.page_source
-    for x in range(1, page_count):
-        driver.get(driver.current_url + '&show=' + str(x * 24))
-        yield driver.page_source
+# Web requests
+async def login(session):
+    await session.post('https://myanimelist.net/login.php', data={
+        'action': 'login',
+        'username': input('Your myanimelist.net username: '),
+        'password': getpass('Your myanimelist.net password: '),
+    })
 
-def fetch_users(page) -> List[str]:
-    return re.findall(r'(?<=<div class="picSurround"><a href=").+?(?=">)', page)
+async def users_from_search_page(session, page_num, name, location, older, younger, gender):
+    params = {
+        'q': name,
+        'loc': location,
+        'agelow': older,
+        'agehigh': younger,
+        'g': gender,
+        'show': str(page_num * 24),
+    }
+    async with session.get('https://myanimelist.net/users.php', params=params) as resp:
+        return re.findall(r'(?<=<div class="picSurround"><a href=").+?(?=">)',
+                          await resp.text())
+
+async def page_text(session, url):
+    async with session.get(url) as resp:
+        return await resp.text()
+
 
 User = namedtuple('User', ['name', 'last_online', 'gender', 'birthday', 'joined',
                            'location', 'shared', 'affinity', 'friend_count', 'days',
                            'mean_score', 'completed'])
 
-def get_user_data(driver, url) -> User:
-    url = 'https://myanimelist.net' + url
-    driver.get(url)
-    p = driver.page_source
+# Scraping user data
+def get_user_data(p) -> User:
     return User(
         name=safe_re_search(r"<span.*?>\s*(.*?)'s Profile", p),
         last_online=without_seconds(mal_to_datetime(safe_re_search(r"Last Online</span>.*?>(.*?)</span>", p))),
@@ -87,12 +134,20 @@ def get_user_data(driver, url) -> User:
         completed=safe_int(safe_re_search(r'Completed</a><span class="di-ib fl-r lh10">([\d,]+)', p)),
     )
 
+def scrape_affinity(page):
+    match = re.search(r'<div class="bar-outer-negative ar"><.*?>[-]?([-]?\d+\.*\d*)%'
+                      r'.*?</span></div>\s*<div class="bar-outer-positive al"><.*?>.*?(\d+\.*\d*)%', page)
+    if match:
+        return float(match.group(1)) or float(match.group(2))
+
 def safe_re_search(*args, **kwargs) -> Optional[str]:
     match = re.search(*args, **kwargs)
     if match is None:
         return None
     return match.group(1)
 
+
+# Helper functions
 def mal_to_datetime(mal_time: str) -> Optional[datetime]:
     if not mal_time:
         return None
@@ -134,12 +189,6 @@ def to_date(date_time: Optional[datetime]) -> Optional[str]:
     if date_time:
         return date_time.date().isoformat()
 
-def scrape_affinity(page):
-    match = re.search(r'<div class="bar-outer-negative ar"><.*?>[-]?([-]?\d+\.*\d*)%'
-                      r'.*?</span></div>\s*<div class="bar-outer-positive al"><.*?>.*?(\d+\.*\d*)%', page)
-    if match:
-        return float(match.group(1)) or float(match.group(2))
-
 def safe_int(text: str) -> Optional[int]:
     if text is not None:
         return int(text.replace(',', ''))
@@ -148,6 +197,8 @@ def safe_float(text: str) -> Optional[float]:
     if text is not None:
         return float(text.replace(',', ''))
 
+
+# Database
 def save_to_db(db_path, users):
     db = sqlite3.connect(db_path)
     with db:
@@ -169,4 +220,4 @@ def save_to_db(db_path, users):
                        + ' VALUES (' + ('?,' * len(User._fields))[:-1] + ')', users)
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
